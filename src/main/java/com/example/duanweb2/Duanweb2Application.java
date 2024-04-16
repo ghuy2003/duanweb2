1d66126602bbf97bf921506e8939aa92ce96e0ea
ghuy2003 <2003nguyengiahuy@gmail.com>
2024-04-16 15:10:16 +0700

    duancanhan

---
+++ src/main/java/com/example/duanweb2/Duanweb2Application.java
@@ -0,0 +1,13 @@
+package com.example.duanweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Duanweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Duanweb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/duanweb2/Duanweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duanweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Duanweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
